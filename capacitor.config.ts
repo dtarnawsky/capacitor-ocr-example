700fc26bf41b0917549369250548e30e1039046d
Damian Tarnawsky <damian@ionic.io>
2023-02-23 13:50:16 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'capacitor.ocr.example',
+  appName: 'capacitor-ocr-example',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
