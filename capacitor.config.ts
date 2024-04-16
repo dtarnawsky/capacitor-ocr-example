bbd8b7866e934afb9cf05db3be974cab6301d52e
damian.ionic <damian@ionic.io>
2024-04-16 11:04:20 -0700

    feat: Capacitor 6 support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'capacitor.ocr.example',
   appName: 'capacitor-ocr-example',
-  webDir: 'www/browser'
+  webDir: 'www'
 };
 
 export default config;
